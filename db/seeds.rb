c05ec9bbd5d8794478042d2dcb48c9b67b69c121
Vivian Chen <vnchen92@gmail.com>
2022-06-20 13:38:09 -0400

    deleted demouser component, added demouser to db instead

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,6 +10,15 @@ User.destroy_all
 
 u1 = User.create(
     id: 1,
+    name: 'demouser',
+    birthdate: '2022/06/17',
+    weight: 0,
+    email: 'demouser@demo.com',
+    password: "demouser"
+)
+
+u2 = User.create(
+    id: 2,
     name: 'Vivian Chen',
     birthdate: '1992/08/09',
     weight: 125,
