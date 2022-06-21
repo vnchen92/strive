eeb724e797c673e30a1bae6941994ea2bc7bae3a
Vivian Chen <vnchen92@gmail.com>
2022-06-20 22:08:52 -0400

    Halfway done with CSS on homepage

--- frontend/components/splash/welcome.jsx
+++ frontend/components/splash/welcome.jsx
@@ -11,9 +11,9 @@ const Welcome = ({currentUser, logout, login, location}) => {
     let linkToRender;
 
     if (currentPath === '/' || currentPath === '/signup') {
-        linkToRender = <Link to='/login'><button className='nav-bar-btn'>LoginButton</button></Link>
+        linkToRender = <Link to='/login'><button className='nav-bar-btn-login'>Log In</button></Link>
     } else if (currentPath === '/login') {
-        linkToRender = <Link to='/signup'><button className='nav-bar-btn'>SignUpButton</button></Link>
+        linkToRender = <Link to='/signup'><button className='nav-bar-btn-signup'>Sign Up</button></Link>
     }
 
     const history = useHistory();
@@ -31,10 +31,10 @@ const Welcome = ({currentUser, logout, login, location}) => {
                 currentUser ? (
                     // PLACEHOLDER FOR DASHBOARD
                     <>
-                        <header class='header-container'>
+                        <header className='header-container'>
                             <nav className='nav-bar'>
                                 <div className='nav-bar-title-container'>
-                                    <Link to='/'><h1 className='nav-bar-title'>Strive</h1></Link>
+                                    <Link to='/'><h1 className='nav-bar-title'>STRIVE</h1></Link>
                                 </div>
                                 <p>Hello, {currentUser.name}</p>
                                 <div>
@@ -46,16 +46,16 @@ const Welcome = ({currentUser, logout, login, location}) => {
                     // PLACEHOLDER FOR DASHBOARD
                 ) : (
                     <div>
-                        <header class='header-container'>
+                        <header className='header-container'>
                             <nav className='nav-bar'>
                                 <div className='nav-bar-title-container'>
-                                    <Link to='/'><h1 className='nav-bar-title'>Strive</h1></Link>
+                                    <Link to='/'><h1 className='nav-bar-title'>STRIVE</h1></Link>
                                 </div>
-                                <div className='nav-bar-btn-container'>
-                                    {linkToRender}
-                                </div>
+                                {linkToRender}
                             </nav>
                         </header>
+                        <h2>The #1 app for runners and cyclists</h2>
+                        <img alt="" srcset="https://d3nn82uaxijpm6.cloudfront.net/assets/website/show_simple/devices-header-3349320fa849e6a297a3b0d64a6dfdef7307b0fe50f6329a459a0105b76ffff8.jpg 1x, https://d3nn82uaxijpm6.cloudfront.net/assets/website/show_simple/devices-header@2x-5e0be9810fb0366d567d4d53f19c61b4d7bf4275c5b13356456efc4b5e16fc67.jpg 2x" src="https://d3nn82uaxijpm6.cloudfront.net/assets/website/show_simple/devices-header-3349320fa849e6a297a3b0d64a6dfdef7307b0fe50f6329a459a0105b76ffff8.jpg"></img>
                         {
                             currentPath === '/' ? (
                                 <div>
