923451fea5a349d5fcbab0ea126c10def9db97a0
Vivian Chen <vnchen92@gmail.com>
2022-06-23 18:39:19 -0400

    added CSS for modal

--- frontend/components/session_form/rest_of_signup_form.jsx
+++ frontend/components/session_form/rest_of_signup_form.jsx
@@ -7,38 +7,46 @@ const RestOfSignupForm = ({ firstName, lastName, birthdate, weight, handleSubmit
             <h1 className='modal-title'>Create your profile</h1>
             <p className='modal-context'>This will give you a place to store workouts and help your friends find you.</p>
             <div className='modal-inputs-container'>
-                <label className='modal-label'>First Name
-                    <input
-                        className='modal-textbox'
-                        type="text" 
-                        value={firstName} 
-                        onChange={update('firstName')}
-                    />
-                </label>
-                <label className='modal-label'>Last Name
-                    <input 
-                        className='modal-textbox'
-                        type="text" 
-                        value={lastName} 
-                        onChange={update('lastName')}
-                    />
-                </label >
-                <label className='modal-label'>Birthday
-                    <input
-                        className='modal-textbox'
-                        type="date" 
-                        value={birthdate} 
-                        onChange={update('birthdate')}
-                    />
-                </label>
-                <label className='modal-label'>Weight
-                    <input
-                        className='modal-textbox'
-                        type="text" 
-                        value={weight} 
-                        onChange={update('weight')}
-                    />
-                </label>
+                <div className='modal-row'>
+                    <div className='modal-input-container'>
+                    <label className='modal-label'>First Name</label>
+                        <input
+                            className='modal-textbox'
+                            type="text" 
+                            value={firstName} 
+                            onChange={update('firstName')}
+                        />
+                    </div>
+                    <div className='modal-input-container'>
+                    <label className='modal-label'>Birthday</label >
+                        <input
+                            className='modal-textbox'
+                            type="date" 
+                            value={birthdate} 
+                            onChange={update('birthdate')}
+                        />
+                    </div>
+                </div>
+                <div className='modal-row'>
+                    <div className='modal-input-container'>
+                        <label className='modal-label'>Last Name</label >
+                            <input 
+                            className='modal-textbox'
+                            type="text" 
+                            value={lastName} 
+                            onChange={update('lastName')}
+                            />
+                    </div>
+                    <div className='modal-input-container'>
+                    <label className='modal-label'>Weight</label >
+                        <input
+                            className='modal-textbox'
+                            type="text" 
+                            value={weight} 
+                            onChange={update('weight')}
+                        />
+                    </div>
+                </div>
                 <div className='modal-continue-btn-container'>
                     <button className='modal-continue-btn' type='submit' onSubmit={handleSubmit}>Continue</button>
                 </div>
