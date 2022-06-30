7816532391603c1bd5b7b399afab3e7e281ad291
Vivian Chen <vnchen92@gmail.com>
2022-06-30 02:43:38 -0400

    changed api key

--- frontend/util/elevation_api.util.js
+++ frontend/util/elevation_api.util.js
@@ -1,9 +1,8 @@
-export const fetchPathElevation = () => {
+export const fetchPathElevation = (staticMapUrl) => {
     //debugger
     return $.ajax({
         method: 'GET',
-        // url: `https://maps.googleapis.com/maps/api/elevation/json?path=enc:${staticMapUrl}&samples=50&key=${window.MAPS_API_KEY}`
-        url: 'https://maps.googleapis.com/maps/api/elevation/json?path=enc:gz|wFjrobMHYw@g@o@c@}BwAyAaA{@o@eAq@mCcBaHsEwJqG{B}AaCyAQIgBqAeAo@}CsBaHsEuJqGuOeKqAy@i@[e@_@wA}@SKQSq@c@oAy@cAq@bAaDxBeHJ]L]\o@R_@\gAp@mBPu@ZaAx@kCQC@samples=10&key=AIzaSyDpbH2qlI6g7TPx7PAx3tCLKExXmrxJa-M'
+        url: `https://maps.googleapis.com/maps/api/elevation/json?path=enc:${staticMapUrl}&samples=50&key=${window.MAPS_API_KEY}`
     })
 }
 
