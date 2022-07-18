aab23900e18f0cc016ed8d2dc2c70d454d45ede7
Vivian Chen <vnchen92@gmail.com>
2022-07-17 23:01:46 -0400

    added kudos reducer

---
+++ frontend/reducers/kudos_reducer.js
@@ -0,0 +1,26 @@
+import { RECEIVE_KUDOS, RECEIVE_KUDO, REMOVE_KUDO } from "../actions/kudo_actions"
+
+const initialState = {
+
+}
+
+const kudosReducer = (state = initialState, action) => {
+    Object.freeze(state);
+    let nextState = {...state};
+    switch(action.type){
+        case RECEIVE_KUDOS:
+            nextState = {...action.kudos};
+            return nextState;
+        case RECEIVE_KUDO:
+            let currentState = nextState[action.kudo.activityId][action.kudo.id];
+            nextState[action.kudo.activityId][action.kudo.id] = {...currentState, ...action.kudo};
+            return nextState;
+        case REMOVE_KUDO:
+            delete nextState[action.kudo.activityId][action.kudo.id]
+            return nextState;
+        default:
+            return nextState;
+    }
+}
+
+export default kudosReducer;
