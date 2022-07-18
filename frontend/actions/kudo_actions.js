4272038fd79f225f4b9b3f954dd42c18a97c13b1
Vivian Chen <vnchen92@gmail.com>
2022-07-17 22:19:22 -0400

    added kudo actions

---
+++ frontend/actions/kudo_actions.js
@@ -0,0 +1,47 @@
+import * as KudoApiUtil from '../util/kudo_api_util';
+
+export const RECEIVE_ALL_KUDOS = 'RECEIVE_ALL_KUDOS';
+export const RECEIVE_KUDO = 'RECEIVE_KUDO';
+export const REMOVE_KUDO = 'REMOVE_KUDO';
+
+const receiveAllKudos = kudos => {
+    return {
+        type: RECEIVE_ALL_KUDOS,
+        kudos
+    }
+}
+
+const receiveKudo = kudo => {
+    return {
+        type: RECEIVE_KUDO,
+        kudo
+    }
+}
+
+const removeKudo = kudo => {
+    return {
+        type: REMOVE_KUDO,
+        kudo
+    }
+}
+
+export const fetchAllKudos = () => dispatch => {
+    return (
+        KudoApiUtil.fetchAllKudos()
+            .then(res => dispatch(receiveAllKudos(res)))
+    )
+}
+
+export const createKudo = kudo => {
+    return (
+        KudoApiUtil.createKudo(kudo)
+            .then(res => dispatch(receiveKudo(kudo)))
+    )
+}
+
+export const deleteKudo = kudoId => dispatch => {
+    return (
+        KudoApiUtil.deleteKudo(kudoId)
+            .then(res => dispatch(removeKudo(res)))
+    )
+}
