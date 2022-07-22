116ab8e5be7a6f822afec106d6231b594f28d22c
Vivian Chen <vnchen92@gmail.com>
2022-07-22 12:05:39 -0400

    updated comment dates

--- frontend/components/comments/comment.jsx
+++ frontend/components/comments/comment.jsx
@@ -1,7 +1,40 @@
 import React from 'react';
 
-const Comment = ({comment, currentUser, deleteComment}) => {
-    //debugger
+const Comment = ({ comment, currentUser, deleteComment}) => {
+
+    const monthNameToNum = {
+        "Jan": '01',
+        "Feb": '02',
+        "Mar": '03',
+        "Apr": '04',
+        "May": '05',
+        "Jun": '06',
+        "Jul": '07',
+        "Aug": '08',
+        "Sep": '09',
+        "Oct": '10',
+        "Nov": '11',
+        "Dec": '12',
+    }
+
+    const convertMonthNametoNum = date => {
+        let newDate = new Date(date).toString().split(" ").splice(1, 3)
+        let month = monthNameToNum[newDate[0]]
+        debugger
+        newDate[0] = month;
+        return new Date(newDate[2], newDate[0], newDate[1]);
+    }
+
+    const oneDay = 24 * 60 * 60 * 1000;
+    const presentDate = convertMonthNametoNum(comment.createdAt)
+    const activityDate = convertMonthNametoNum(new Date())
+    const daysAgo = Math.round(Math.abs((presentDate - activityDate) / oneDay));
+
+    // const oneDay = 24 * 60 * 60 * 1000;
+    // const presentDate = new Date(comment.createdAt)
+    // const activityDate = new Date(activityPostedOn)
+    // const daysAgo = Math.round(Math.abs((presentDate - activityDate) / oneDay));
+
     return (
         <>
             <img className='comment-user-icon' src={comment.profilePic} alt="" />
@@ -9,7 +42,7 @@ const Comment = ({comment, currentUser, deleteComment}) => {
                 <div className='comment-name-time-delete-container'>
                     <p className='comment-name'>{comment.firstName}&nbsp;{comment.lastName}</p>
                     <div className='comment-time-delete-container'>
-                        <p className='comment-time'>{comment.createdAt}
+                        <p className='comment-time'>{daysAgo} days ago
                         {
                         currentUser.id === comment.userId ? (
                             <button className='comment-delete-btn' onClick={() => deleteComment(comment.id)}>&nbsp;| Delete</button>
