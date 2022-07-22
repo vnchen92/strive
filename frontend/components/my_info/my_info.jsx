3a9b337329a09561c2065fac2c76d321382172a7
Vivian Chen <vnchen92@gmail.com>
2022-07-22 17:10:50 -0400

    Added and styled my portfolio info

--- frontend/components/my_info/my_info.jsx
+++ frontend/components/my_info/my_info.jsx
@@ -2,24 +2,38 @@ import React from 'react';
 
 const MyInfo = props => {
     return (
-        <div className='myinfo-container'>
-            <div className='myinfo-row-container'>
-                <div className='myinfo-icon-name'>
-                    GitHub
+        <>
+            <div className='myinfo-container'>
+                <div className='myinfo-icon-and-label-container'>
+                    <div className='myinfo-icon-and-label-inner-container'>
+                        <img src="/assets/icons8-github-100" alt="" />
+                        GitHub
+                    </div>
                 </div>
-                <div className='myinfo-icon-name'>
-                    LinkedIn
-                </div>
-            </div>
-            <div className='myinfo-row-container'>
-                <div className='myinfo-icon-name'>
-                    AngelList
-                </div>
-                <div className='myinfo-icon-name'>
-                    Portfolio
+                <div className='myinfo-icon-and-label-container'>
+                    <div className='myinfo-icon-and-label-inner-container'>
+                        <img src="/assets/icons8-linkedin-100" alt="" />
+                        LinkedIn
+                    </div>
                 </div>
             </div>
-        </div>
+            <div className='myinfo-container-2'>
+
+                    <div className='myinfo-icon-and-label-container'>
+                        <div className='myinfo-icon-and-label-inner-container'>
+                            <img src="/assets/icons8-angellist-100" alt="" />
+                            AngelList
+                        </div>
+                    </div>
+                    <div className='myinfo-icon-and-label-container'>
+                        <div className='myinfo-icon-and-label-inner-container'>
+                            <img src="/assets/icons8-girl-64" alt="" />
+                            Portfolio
+                        </div>
+                    </div>
+                
+            </div>
+        </>
     )
 }
 
