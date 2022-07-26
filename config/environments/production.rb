d5ff83ea543e34b65ac29456acb07b502d32d2a8
Vivian Chen <vnchen92@gmail.com>
2022-07-26 00:53:51 -0400

    heroku push

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -26,6 +26,8 @@ Rails.application.configure do
   config.assets.js_compressor = Uglifier.new(:harmony => true)
   # config.assets.css_compressor = :sass
 
+  config.assets.js_compressor = :terser
+
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = true
 
