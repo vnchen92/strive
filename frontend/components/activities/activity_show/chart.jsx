84689709efe5d4dc8ab7d27ae018285d5cd93956
Vivian Chen <vnchen92@gmail.com>
2022-08-13 22:59:51 -0400

    updated x-axis labels

--- frontend/components/activities/activity_show/chart.jsx
+++ frontend/components/activities/activity_show/chart.jsx
@@ -23,35 +23,37 @@ import { Line } from "react-chartjs-2";
 
 const LineChart = ({elevationData}) => {
 	const data = {
-		labels: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
+		labels: ["", "", "", "", "", "", "", "", "", ""],
 		datasets: [{
 			data: elevationData,
 			//backgroundColor: 'rgb(255, 99, 132)',
 			borderColor: 'rgb(255, 99, 132)'
 		  }]
 	}
+
+	const options = {
+		scales: {
+			y: {
+				beginAtZero: true
+			}
+		},
+		plugins: {
+			title: {
+			  display: true,
+			  text: "Elevation"
+			},
+			legend: {
+			  display: false
+			  //position: "bottom"
+		   	}
+		}
+	}
 	
 	return (
 		<div>
 			<Line
 				data={data}
-				options={{
-				   scales: {
-					y: {
-						beginAtZero: true
-					}
-				   },
-				   plugins: {
-					title: {
-					  display: true,
-					  text: "Elevation"
-					},
-					legend: {
-					  display: false
-					  //position: "bottom"
-				   }
-				  }
-				}}
+				options={options}
 			/>
 		</div>
 	)
