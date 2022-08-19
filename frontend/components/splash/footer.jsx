76dd8c77769eaaac1a0eea6e7c6dcfbcad1b6643
Vivian Chen <vnchen92@gmail.com>
2022-08-18 20:52:31 -0400

    updated footer links

--- frontend/components/splash/footer.jsx
+++ frontend/components/splash/footer.jsx
@@ -5,10 +5,10 @@ const Footer = props => {
         <div className='footer-container'>
             <h1>STRIVE</h1>
             <div className='footer-onlinepresence-container'>
-                <li>GitHub</li>
-                <li>LinkedIn</li>
-                <li>AngelList</li>
-                <li>Portfolio</li>
+                <li><a href="https://github.com/vnchen92" target="_blank">GitHub</a></li>
+                <li><a href="https://www.linkedin.com/in/vivian-chen-8082169a/" target="blank">LinkedIn</a></li>
+                <li><a href="https://angel.co/u/vivian-chen-42" target="_blank">AngelList</a></li>
+                <li><a href="https://vnchen92.github.io/" target="_blank">Portfolio</a></li>
             </div>
         </div>
     )
